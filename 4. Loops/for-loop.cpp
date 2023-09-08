7d9841f3c681e22133a675045cd796889f1c3d9b
Shivam Kumar <90146531+Shivam1320@users.noreply.github.com>
2023-09-08 11:08:18 +0530

    Add files via upload

---
+++ 4. Loops/for-loop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // for(int i = 0; i<5; i++){
+    //     cout<< i <<endl;
+    // }
+    for(int i = 5; i>0; i--){
+        cout<< i <<endl;
+    }
+
+    return 0;
+}
