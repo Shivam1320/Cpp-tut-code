6752640a077776fb154f28b2420d21a5a7857c90
Shivam Kumar <90146531+Shivam1320@users.noreply.github.com>
2023-09-12 10:20:36 +0530

    Add files via upload

---
+++ 10. Pointers/Intro.cpp
@@ -0,0 +1,41 @@
+// Pointer   --> Pointer in C++ is a variable that stores the address of another variable.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+
+    // int a = 5;
+    // int b = 6;
+    // cout<< "address of a" << &a <<endl;
+    // cout<< "address of b" << &b <<endl;
+
+    // int a = 5;
+    // //create an pointer
+    // int* ptr = &a;
+
+    // //accescc the value ptr is pointing to
+    // cout<< "Address of a is: " << &a <<endl;
+    // cout << "value stored at ptr is " << ptr <<endl;
+    // cout << "value ptr is pointing to is " << *ptr <<endl;
+    // cout << "address of ptr is " << &ptr <<endl;
+
+    // int a = 5;
+    // int* ptr = &a;
+    // cout << sizeof(ptr) <<endl;
+
+
+    // char ch = 'b';
+    // char* c = &ch;
+    // cout << sizeof(c) <<endl;
+
+
+    // double dtr = 5.03;
+    // double* d = &dtr;
+    // cout << sizeof(d) <<endl;
+
+
+
+return 0;
+}
---
+++ 10. Pointers/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+ // int a = 5;
+    // //create an pointer
+    // int* ptr = &a;
+
+    // //accescc the value ptr is pointing to
+    // cout <<*ptr <<endl;
